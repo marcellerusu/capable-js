1daf0a8df47296b1a12a7e55e5bac09a90eb648c
MarcelRusu <marcelrusu@protonmail.com>
2022-10-21 18:54:06 -0400

    rm dist & use state inline

--- test.tsx
+++ test.tsx
@@ -3,14 +3,12 @@ import { h } from "./html.js";
 import { Signal } from "./signal.js";
 import http from "./http.js";
 
-let count = new Signal(0);
-
 function* Hello() {
   yield <div>loading...</div>;
   let { title, completed } = yield http.get(
     "https://jsonplaceholder.typicode.com/todos/1"
   );
-  let $count = yield count;
+  let $count = yield new Signal(0);
   yield (
     <p>
       title:{title} - completed:{completed}
