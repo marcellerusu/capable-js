a8651e0b26590b6f22937e511def86c497ed64e0
MarcelRusu <marcelrusu@protonmail.com>
2022-10-30 11:02:35 -0500

    event_handler -> form_utils

---
+++ effects/form_utils.ts
@@ -0,0 +1,24 @@
+import * as capable from "../index.js";
+class FormSubmission {
+  form: HTMLFormElement;
+  constructor(target: HTMLFormElement) {
+    this.form = target;
+  }
+}
+
+capable.runtime.register(FormSubmission, (_component, { form }) => {
+  return new Promise((resolve) => {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      resolve(Object.fromEntries(new FormData(form)));
+    });
+  });
+});
+
+let form_utils = {
+  on_submit(form: HTMLFormElement) {
+    return new FormSubmission(form);
+  },
+};
+
+export default form_utils;
--- test.tsx
+++ test.tsx
@@ -1,73 +1,7 @@
 import * as capable from "./index.js";
 import { h } from "./effects/html.js";
-import lock from "./effects/lock.js";
 import css from "./effects/css.js";
-import http from "./effects/http.js";
-import on from "./effects/event_handler.js";
-
-async function* Hello() {
-  let name = "";
-
-  // let name_loop = yield interval.each_second(() => {
-  //   console.log({ name });
-  // });
-
-  let form_style = yield css.class`
-    color: red;
-  `;
-
-  let $name_lock = yield lock.new();
-  for await (let _ of $name_lock) {
-    yield (
-      <form on:submit$preventDefault class={form_style}>
-        What's your name?
-        <input type="checkbox" on:click$preventDefault />
-        <input type="text" on:input={(e) => (name = e.target.value)} />
-        <button type="submit" on:click={() => $name_lock.release()}>
-          Submit
-        </button>
-      </form>
-    );
-  }
-
-  // yield interval.stop(name_loop);
-
-  yield <div>Hello, {name}!</div>;
-}
-
-async function* NameForm() {
-  let name = "";
-  let $name_lock = yield lock.new();
-  for await (let _ of $name_lock) {
-    console.log($name_lock);
-    yield (
-      <form on:submit$preventDefault>
-        What's your name?
-        <input type="text" on:input={(e) => (name = e.target.value)} />
-        <button type="submit" on:click={() => $name_lock.release()}>
-          Submit
-        </button>
-      </form>
-    );
-  }
-  // note this is a return, not a yield
-  return name;
-}
-
-class Log {
-  msg: string;
-  constructor(msg: string) {
-    this.msg = msg;
-  }
-}
-
-capable.runtime.register(Log, (_component, log_cmd) => {
-  console.log(log_cmd.msg);
-});
-
-async function* Main() {
-  yield new Log("hello there");
-}
+import form_utils from "./effects/form_utils.js";
 
 async function* Form() {
   let form_style = yield css.class`
@@ -86,7 +20,7 @@ async function* Form() {
     </form>
   );
 
-  let { name } = yield on.submit(form);
+  let { name } = yield form_utils.on_submit(form);
 
   yield css.global`
     color: hotpink;
